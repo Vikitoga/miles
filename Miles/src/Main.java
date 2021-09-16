caeec28470f6c1de74e299758ce4e9547e52f8df
Viktoria <vikitoga@gmail.com>
2021-09-16 11:48:41 +0300

    Project Miles added

---
+++ Miles/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 2923;
+        int moneyForMile = 20;
+        int bonusMiles = ticketPrice/moneyForMile;
+        System.out.println(bonusMiles);
+
+    }
+}
